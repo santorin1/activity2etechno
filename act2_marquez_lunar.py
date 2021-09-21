8361de3d0d0483e9b83ab514872cb52e4380216c
xiandee <1801899@ub.edu.ph>
2021-09-21 09:22:23 +0000

    Second Part-Pass or Fail

--- act2_marquez_lunar.py
+++ act2_marquez_lunar.py
@@ -1,3 +1,7 @@
 prelims,midterm,semis,finals = int(input("Prelims :")),int(input("Midterms :")),int(input("Semis : ")),int(input("Finals : "))
 ave= (prelims+midterm+semis+finals)/4
 print ("The average is ", ave)
+if ave >=75:
+    print ("Passed")
+else:
+    print ("Failed")
