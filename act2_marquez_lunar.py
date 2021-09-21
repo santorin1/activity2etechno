f052f25fd7a2b030719434faa27d0572cb39f8c8
xiandee <1801899@ub.edu.ph>
2021-09-21 09:23:49 +0000

    Third Part-Final Grade

--- act2_marquez_lunar.py
+++ act2_marquez_lunar.py
@@ -5,3 +5,15 @@ if ave >=75:
     print ("Passed")
 else:
     print ("Failed")
+if ave <= 100 and ave >=99:
+    print ("Equivalent Grade :A")
+elif ave <= 98 and ave >=90:
+    print ("Equivalent Grade :B") 
+elif ave <= 89 and ave >=80:
+    print ("Equivalent Grade :C")
+elif ave <= 79 and ave >=71:
+    print ("Equivalent Grade :D")
+elif ave <= 70 and ave >=61:
+    print ("Equivalent Grade :E") 
+else:
+    print ("Equivalent Grade :F")
