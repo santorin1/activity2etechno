94436d9ab01e2eebad2daf9de9289a9574d6bcf5
xiandee <1801899@ub.edu.ph>
2021-09-21 09:19:50 +0000

    First part-Compute Average

---
+++ act2_marquez_lunar.py
@@ -0,0 +1,3 @@
+prelims,midterm,semis,finals = int(input("Prelims :")),int(input("Midterms :")),int(input("Semis : ")),int(input("Finals : "))
+ave= (prelims+midterm+semis+finals)/4
+print ("The average is ", ave)
